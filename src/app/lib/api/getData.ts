79f2930b1def6dbd2512c95fea35e14bc1e3379f
sepehr <gramysphr4@gmail.com>
2026-06-19 20:18:57 +0330

    update

--- src/app/lib/api/getData.ts
+++ src/app/lib/api/getData.ts
@@ -1,35 +1,23 @@
 
-
 export type Product = {
   id: number;
   title: string;
   image: string;
   price: number;
-  description: string;
-  category: string;
-  rating: {
+  description:string;
+  category:string;
+    rating: {
     rate: number;
     count: number;
   };
 };
 
 export async function GetData(): Promise<Product[]> {
-  try {
-    const res = await fetch("https://fakestoreapi.com/products", {
-      next: {
-        revalidate: 60,
-      },
-    });
+  const res = await fetch("https://fakestoreapi.com/products", {
+    next: { revalidate: 60 },
+  });
 
-    if (!res.ok) {
-      console.error("API response not ok:", res.status);
-      return [];
-    }
+  const data = await res.json();
 
-    return await res.json();
-  } catch (error) {
-    console.error("GetData error:", error);
-    return [];
-  }
+return data
 }
-
